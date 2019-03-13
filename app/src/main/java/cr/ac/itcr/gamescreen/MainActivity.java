eb01d786623fc100dcd42d38e15c0ab935b98c8e
Ignacio Gomez <IgnacioGomezCh@users.noreply.github.com>
2019-03-13 11:02:48 -0600

    Added pictures and screen display

--- app/src/main/java/cr/ac/itcr/gamescreen/MainActivity.java
+++ app/src/main/java/cr/ac/itcr/gamescreen/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setContentView(R.layout.activity_main);
-        Log.e("Width", "sxsx");
         GridLayout board = findViewById(R.id.boardLayout);
 
 
@@ -27,22 +26,29 @@ public class MainActivity extends AppCompatActivity {
         Point size = new Point();
         display.getSize(size);
         int width = size.x;
-        int height = size.y;
-        Log.e("Width", "" + width);
-        Log.e("height", "" + height);
 
-
-        int cellSize =  40;//board.getWidth();
+        int cellSize =  (width - 16)/12 - 1;//board.getWidth();
         for(int i = 0; i<288; i++) {
             View img = new View(this);
 
             img.setLayoutParams(new ViewGroup.LayoutParams(cellSize, cellSize));
-            if(i%2==0)
-                img.setBackgroundColor(Color.CYAN);
+            if (i % 24 == 23 || i % 12 == 0 || i < 11)
+                img.setBackgroundColor(Color.GRAY);
             else
-                img.setBackgroundColor(Color.RED);
+                img.setBackgroundColor(Color.BLACK);
             board.addView(img, i);
+
         }
+
+        View pieceCreator = new View(this);
+        pieceCreator.setLayoutParams(new ViewGroup.LayoutParams(cellSize, cellSize));
+        pieceCreator.setBackgroundColor(Color.RED);
+        board.addView(pieceCreator, 148);
+
+
+        System.out.println("------------------------" + (11%12));
+
+
         /*
         int count = board.getChildCount();
         System.out.println(count);
@@ -59,5 +65,5 @@ public class MainActivity extends AppCompatActivity {
     public void fillBackground(){
 
     }
-    
+
 }
